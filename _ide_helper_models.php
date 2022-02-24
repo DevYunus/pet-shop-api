2899db633903f96b4de7c0bc62b229546e0ccdcb
Yunus Shaikh <s.yunus0493@gmail.com>
2022-02-24 18:23:38 +0530

    Added middleware

---
+++ _ide_helper_models.php
@@ -0,0 +1,273 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Brand
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\BrandFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereUuid($value)
+ */
+	class Brand extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Product[] $products
+ * @property-read int|null $products_count
+ * @method static \Database\Factories\CategoryFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereUuid($value)
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $order_status_id
+ * @property int $payment_id
+ * @property string $uuid
+ * @property array $address
+ * @property float|null $delivery_fee
+ * @property float $amount
+ * @property int|null $shipped_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\OrderStatus $orderStatus
+ * @property-read \App\Models\Payment $payment
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Product[] $products
+ * @property-read int|null $products_count
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\OrderFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereDeliveryFee($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereOrderStatusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePaymentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereShippedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order withOrderStatus($orderStatus)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order withPayment($payment)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order withSort($sorting)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order withUser($user)
+ */
+	class Order extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\OrderStatus
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\OrderStatusFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereUuid($value)
+ */
+	class OrderStatus extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Payment
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $type
+ * @property array $details
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\PaymentFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereDetails($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereUuid($value)
+ */
+	class Payment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property string $category_uuid
+ * @property string $uuid
+ * @property string $title
+ * @property float $price
+ * @property string $description
+ * @property array $metadata
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category|null $category
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read int|null $orders_count
+ * @method static \Database\Factories\ProductFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Product onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCategoryUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereMetadata($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product withCategory($category)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product withPrice($price)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product withSort($sorting)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product withTitle($title)
+ * @method static \Illuminate\Database\Query\Builder|Product withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Product withoutTrashed()
+ */
+	class Product extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Promotion
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property string $content
+ * @property array $metadata
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\PromotionFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereMetadata($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promotion whereUuid($value)
+ */
+	class Promotion extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property bool $is_admin
+ * @property int|null $email_verified_at
+ * @property string $password
+ * @property string|null $avatar
+ * @property string $address
+ * @property string $phone_number
+ * @property bool $is_marketing
+ * @property int|null $last_login_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read int|null $orders_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsMarketing($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withAddress($address)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withEmail($email)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withFirstName($firstName)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withIsMarketing($isMarketing)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withPhone($phone)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withSort($sorting)
+ */
+	class User extends \Eloquent {}
+}
+
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
-use App\Http\Resources\ProductCollection;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,50 +20,50 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
-Route::post('admin/login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('admin/login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('admin/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::post('admin/logout',
+            [App\Http\Controllers\AuthController::class, 'logout'])->middleware('admin');
 
-    // Category Routes
-    Route::controller(CategoryController::class)->group(function () {
-        Route::get('categories','index');
-        Route::post('category/create', 'store');
-        Route::get('category/{category:uuid}', 'show');
-        Route::delete('category/{category:uuid}', 'destroy');
+        // Category Routes
+        Route::controller(CategoryController::class)->group(function () {
+            Route::get('categories', 'index');
+            Route::post('category/create', 'store')->middleware('admin');
+            Route::get('category/{category:uuid}', 'show');
+            Route::delete('category/{category:uuid}', 'destroy')->middleware('admin');
+        });
+
+        // Product Routes
+        Route::controller(ProductController::class)->group(function () {
+            Route::get('products', 'index');
+            Route::post('product/create', 'store')->middleware('admin');
+            Route::put('product/{product:uuid}', 'update')->middleware('admin');
+            Route::get('product/{product:uuid}', 'show');
+            Route::delete('product/{product:uuid}', 'destroy')->middleware('admin');
+        });
+
+        //Order routes
+        Route::controller(OrderController::class)->group(function () {
+            Route::get('orders', 'index');
+            Route::post('order/create', 'store');
+            Route::put('order/{order:uuid}', 'update');
+            Route::get('order/{order:uuid}', 'show');
+            Route::delete('order/{order:uuid}', 'destroy');
+        });
+
+        //User routes
+        Route::controller(UserController::class)->group(function () {
+            Route::get('user/orders', 'userOrders');
+            Route::get('user/{user:uuid}', 'show');
+            Route::delete('user/{user:uuid}', 'destroy');
+        });
+
+        //Admin routes
+        Route::controller(AdminController::class)->group(function () {
+            Route::get('admin/user-listing', 'index');
+            Route::delete('user-delete/{user:uuid}', 'destroy');
+        });
     });
-
-    // Product Routes
-    Route::controller(ProductController::class)->group(function () {
-        Route::get('products','index');
-        Route::post('product/create', 'store');
-        Route::put('product/{product:uuid}', 'update');
-        Route::get('product/{product:uuid}', 'show');
-        Route::delete('product/{product:uuid}', 'destroy');
-    });
-
-    //Order routes
-    Route::controller(OrderController::class)->group(function () {
-        Route::get('orders','index');
-        Route::post('order/create', 'store');
-        Route::put('order/{order:uuid}', 'update');
-        Route::get('order/{order:uuid}', 'show');
-        Route::delete('order/{order:uuid}', 'destroy');
-    });
-
-    //User routes
-    Route::controller(UserController::class)->group(function () {
-        Route::get('user/orders','userOrders');
-        Route::get('user/{user:uuid}', 'show');
-        Route::delete('user/{user:uuid}', 'destroy');
-
-    });
-
-    //Admin routes
-    Route::controller(AdminController::class)->group(function () {
-        Route::get('admin/user-listing','index');
-        Route::delete('user-delete/{user:uuid}', 'destroy');
-    });
-});
 });
 
